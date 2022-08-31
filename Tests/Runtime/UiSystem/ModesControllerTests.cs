028165ddf978b8e9357c3c94d21cb937fdd88edc
Maxim Epishev <epishev.m@mail.ru>
2022-08-31 23:39:25 +0300

    Added ModalLogicControllerTests

--- Tests/Runtime/UiSystem/ModesControllerTests.cs
+++ Tests/Runtime/UiSystem/ModesControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using EM.UI;
 using NUnit.Framework;
+using UnityEngine;
 
 internal sealed class ModalLogicControllerTests
 {
@@ -44,6 +45,90 @@ internal sealed class ModalLogicControllerTests
 
 		// Assert
 		Assert.IsTrue(actual);
+	}
+
+	[Test]
+	public void ModalLogicController_Add_TryGetPanelView_True()
+	{
+		// Arrange
+		var modesController = new ModalLogicController();
+		var gameObject = new GameObject();
+		var panelView = gameObject.AddComponent<PanelViewTest>();
+
+		// Act
+		modesController.Add(panelView, Modes.None);
+		var actual = modesController.TryGetPanelView<PanelViewTest>(out var actualPanelView);
+
+		// Assert
+		Assert.IsTrue(actual);
+		Assert.NotNull(actualPanelView);
+	}
+
+	[Test]
+	public void ModalLogicController_AddModal_TryGetPanelView_True()
+	{
+		// Arrange
+		var modesController = new ModalLogicController();
+		var gameObject = new GameObject();
+		var panelView = gameObject.AddComponent<PanelViewTest>();
+
+		// Act
+		modesController.Add(panelView, Modes.Modal);
+		var actual = modesController.TryGetPanelView<PanelViewTest>(out var actualPanelView);
+
+		// Assert
+		Assert.IsTrue(actual);
+		Assert.NotNull(actualPanelView);
+	}
+
+	[Test]
+	public void ModalLogicController_Add_TryGetPanelView_False()
+	{
+		// Arrange
+		var modesController = new ModalLogicController();
+
+		// Act
+		var actual = modesController.TryGetPanelView<PanelViewTest>(out var actualPanelView);
+
+		// Assert
+		Assert.IsFalse(actual);
+		Assert.Null(actualPanelView);
+	}
+
+	[Test]
+	public void ModalLogicController_Remove()
+	{
+		// Arrange
+		var modesController = new ModalLogicController();
+		var gameObject = new GameObject();
+		var panelView = gameObject.AddComponent<PanelViewTest>();
+
+		// Act
+		modesController.Add(panelView, Modes.None);
+		modesController.Remove(panelView);
+		var actual = modesController.TryGetPanelView<PanelViewTest>(out var actualPanelView);
+
+		// Assert
+		Assert.IsFalse(actual);
+		Assert.Null(actualPanelView);
+	}
+
+	[Test]
+	public void ModalLogicController_RemoveModal()
+	{
+		// Arrange
+		var modesController = new ModalLogicController();
+		var gameObject = new GameObject();
+		var panelView = gameObject.AddComponent<PanelViewTest>();
+
+		// Act
+		modesController.Add(panelView, Modes.Modal);
+		modesController.Remove(panelView);
+		var actual = modesController.TryGetPanelView<PanelViewTest>(out var actualPanelView);
+
+		// Assert
+		Assert.IsFalse(actual);
+		Assert.Null(actualPanelView);
 	}
 
 	#region Nested
